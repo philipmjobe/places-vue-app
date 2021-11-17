71d9e03ac4849869d7d02cea2d2bc02cc477142f
Philip <philipmjobe@gmail.com>
2021-11-17 09:39:46 -0500

    added html logic to home.vue

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -6,6 +6,32 @@
     <p>Address:</p>
     <input type="text" v-model="newPlaceParams.address" />
     <p></p>
+    <ul>
+      <li v-for="error in errors" v-bind:key="error">{{ error }}</li>
+    </ul>
+    <p></p>
+    <button v-on:click="createPlace()">Create Place</button>
+    <div v-for="place in places" v-bind:key="place.id">
+      <h2>{{ place.name }}</h2>
+      <p>{{ place.address }}</p>
+      <button v-on:clck="showPlace(place)">Update</button>
+      <dialog id="place-details">
+        <form method="dialog">
+          <h2>Place Update</h2>
+          <p>
+            Name:
+            <input type="text" v-model="currentPlace.name" />
+          </p>
+          <p>
+            Address:
+            <input type="text" v-model="currentPlace.address" />
+          </p>
+          <button v-on:click="updatePlace(currentPlace)">Update Place</button>
+          <button v-on:click="destroyPlace(currentPlace)">Delete Place</button>
+          <button>Close</button>
+        </form>
+      </dialog>
+    </div>
   </div>
 </template>
 
